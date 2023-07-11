72627938902c64df223fca218c6b74731a4a0e88
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-07-11 19:27:26 +0530

    push

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ const app = express()
 app.use(express.json());
 
 app.use(cors());
-
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+      return res.sendStatus(401);
+    }
+  
+    jwt.verify(token, 'secret', (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  };
+  
 app.use('/auth', userRouter)
 
 mongoose.connect(`mongodb+srv://rohannsingh987:newsapp@cluster0.nyrk1k7.mongodb.net/?retryWrites=true&w=majority`)
@@ -16,3 +31,4 @@ mongoose.connect(`mongodb+srv://rohannsingh987:newsapp@cluster0.nyrk1k7.mongodb.
 
 const port = 1234
 app.listen(port, ()=> console.log(`Server Running on ${port}`));
+
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,45 +1,43 @@
-import  express  from 'express';
-import jwt from "jsonwebtoken"
-// import bcrypt from "bcrypt"
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
-router.post('/register', async(req, res) => {
-    const {username, password} = req.body;
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
 
-    const user = await UserModel.findOne({ username: username})
+  const user = await UserModel.findOne({ username: username });
 
-    if(user)
-    {
-        return res.json("User already exists")
-    }
+  if (user) {
+    return res.json('User already exists');
+  }
 
-    // const hassPass = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new UserModel({username, password});
-    await newUser.save();
-    
-    res.json(` User Registerd Successfully`);
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
 
+  res.json('User Registered Successfully');
 });
 
-router.post('/login', async(req,res) => {
-    const {username, password} = req.body;
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
-    const user = await UserModel.findOne({username});
-    if(!user)
-    {
-        return res.json({"message" : "User Doesn't Exist"})
-    }
+  const user = await UserModel.findOne({ username });
+  if (!user) {
+    return res.json({ message: "User Doesn't Exist" });
+  }
 
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.json({ message: 'Invalid username or password' });
+  }
 
-    const token = jwt.sign({id: user._id }, "secret")
+  const token = jwt.sign({ id: user._id }, 'secret');
 
-    res.json({token, userId: user._id})
-
-
+  res.json({ token, userId: user._id });
 });
 
- 
-export {router as userRouter};
+export { router as userRouter };
